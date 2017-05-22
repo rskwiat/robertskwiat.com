d6495f72c87842a002320414449a7f7ea6549616
Robert Skwiat <rskwiat@gmail.com>
2017-05-22 15:16:59 -0400

    updating build scripts, files and server to send over txt files

--- server/app.js
+++ server/app.js
@@ -5,8 +5,18 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('*', (request, response) => {
-  response.sendFile(path.resolve(__dirname, '../public', 'index.html'));
+app.get('/robots.txt', (req, res) => {
+  res.type('text/plain');
+  res.sendFile(path.resolve(__dirname, '../public', 'robots.txt'));
+});
+
+app.get('/humans.txt', (req, res) => {
+  res.type('text/plain');
+  res.sendFile(path.resolve(__dirname, '../public', 'humans.txt'));
+});
+
+app.get('*', (request, res) => {
+  res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
 });
 
 module.exports = app;
