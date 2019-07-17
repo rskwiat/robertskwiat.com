feba4aaffd2a1d4d705b0bfa1e3cd55fea29a895
Robert Skwiat <robert.skwiat@mastercard.com>
2019-07-17 15:34:03 -0400

    updating resume view

--- src/views/Resume.js
+++ src/views/Resume.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div><h1>Resume Page Soon</h1></div>
+    <div>
+      <div className="container">
+        <h1>Resume Page Soon</h1>
+      </div>  
+    </div>
   );
 }
 
