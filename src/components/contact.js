75d88b97f9f50eb0cac1588af82458449cf55017
Robert Skwiat <robert.skwiat@mastercard.com>
2019-07-17 13:15:48 -0400

    working on contact area

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from 'scss/contact.module.scss';
+import { Github, LinkedIn, Email } from 'components/icons';
+
+const items = [
+  {
+    'title': 'Github',
+    'link': 'https://github.com/rskwiat'
+  },
+  {
+    'title': 'LinkedIn',
+    'link': 'https://linkedin.com/in/robertskwiat'
+  },
+  {
+    'title': 'Email',
+    'link': 'mailto:rob@robertskwiat.com'
+  }
+];
 
 const Contact = ({ title }) => (
   <section className={styles.contact}>
     <div className="container">
       <h2>{title}</h2>
+      <div className={styles.social}>
+        {items.map((item, i) => {
+          switch(item.title) {
+            case 'Github':
+              return <Github height="75px" width="75px" link={item.link} />
+            case 'LinkedIn':
+              return <LinkedIn height="75px" width="75px" link={item.link} />
+            case 'Email':
+              return <Email height="75px" width="75px" link={item.link} />
+            default:
+              return <div />
+          }
+        })}
+      </div>
     </div>
   </section>
 );
