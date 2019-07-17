5c85a95c30c4cb3da3f04b14a11c3137dae4ba49
Robert Skwiat <robert.skwiat@mastercard.com>
2019-07-17 15:29:00 -0400

    updating develop and home files

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
+import styles from 'scss/home.module.scss';
+
 const Home = () => {
   return (
-    <div><h1>Coming Soon</h1></div>
+    <div className={styles.home}>
+      <div className="container">
+        Logo
+
+        text
+        <h1>Coming Soon</h1>
+      </div>
+    </div>
   );
 }
 
