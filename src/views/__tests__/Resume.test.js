75132aaddadfb4ea177af97adc7584d1658bc08e
Robert Skwiat <robert.skwiat@mastercard.com>
2019-07-25 15:47:46 -0400

    adding in enzyme tests

--- src/views/__tests__/Resume.test.js
+++ src/views/__tests__/Resume.test.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-
+import { mount } from 'enzyme';
 import Resume from '../Resume';
 
 describe('The Home page component', () => {
   test('The component', () => {
-    const component = renderer.create(<Resume />);
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+    const component = mount(<Resume />);
+    expect(component).toMatchSnapshot();
   });
 });
