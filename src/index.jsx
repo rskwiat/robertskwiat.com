01ba9dd623b4f92e545232ced1fa534b3630a107
Robert Skwiat <robert.skwiat@mastercard.com>
2019-12-06 10:37:31 -0500

    testing lint staged

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { render } from "react-dom";
+import React from 'react';
+import { render } from 'react-dom';
 
-import "scss/index.scss";
-import App from "components/App";
+import 'scss/index.scss';
+import App from 'components/App';
 
-const elem = document.getElementById("root");
+const elem = document.getElementById('root');
 
 render(<App />, elem);
