4348bfa9ac851b87530d7fa3d514402e7e164344
Robert Skwiat <rskwiat@gmail.com>
2019-12-05 22:28:45 -0500

    testing prettier formatter

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import Routes from 'constants/routes';
-import styles from 'scss/navigation.module.scss';
+import Routes from "constants/routes";
+import styles from "scss/navigation.module.scss";
 
 const Navigation = () => {
   return (
     <nav className={styles.nav} role="navigation">
       <div className="container">
         <ul>
-          { Routes.map((route, i) => (
-            <li className="nav-item" key={i}><Link to={route.path}>{route.title}</Link></li>
+          {Routes.map((route, i) => (
+            <li className="nav-item" key={i}>
+              <Link to={route.path}>{route.title}</Link>
+            </li>
           ))}
         </ul>
       </div>
