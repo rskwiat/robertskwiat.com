d93ffdc07bc791286634bc37e4c33227da0de053
Robert Skwiat <rskwiat@gmail.com>
2019-12-07 10:07:46 -0500

    updating views

--- src/views/Home.js
+++ src/views/Home.js
@@ -6,29 +6,27 @@ import about from 'constants/about';
 import profileIMG from 'images/headshot.png';
 import styles from 'scss/home.module.scss';
 
-const Home = () => {
-  return (
-    <div className={styles.home}>
-      <div className="container">
-        <div className={styles.profile}>
-          <div className={styles['image']}>
-            <img
-              src={profileIMG}
-              alt="Robert Skwiat"
-              className="img-responsive"
-              tabIndex="0"
-            />
-          </div>
-          <div className={styles['bio']}>
-            <Logo width="300px" />
-            {about.map((text, i) => (
-              <p key={i}>{text.copy}</p>
-            ))}
-          </div>
+const Home = () => (
+  <div className={styles.home}>
+    <div className="container">
+      <div className={styles.profile}>
+        <div className={styles['image']}>
+          <img
+            src={profileIMG}
+            alt="Robert Skwiat"
+            className="img-responsive"
+            tabIndex="0"
+          />
+        </div>
+        <div className={styles['bio']}>
+          <Logo width="300px" />
+          {about.map((text, i) => (
+            <p key={i}>{text.copy}</p>
+          ))}
         </div>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default Home;
--- src/views/Resume.js
+++ src/views/Resume.js
@@ -1,17 +1,17 @@
-import React, { Component } from "react";
-import ReactMarkdown from "react-markdown";
-import Loading from "components/loading";
+import React, { Component } from 'react';
+import ReactMarkdown from 'react-markdown';
+import Loading from 'components/loading';
 
-import resume from "constants/resume.md";
-import styles from "scss/resume.module.scss";
+import resumeMD from 'constants/resume.md';
+import styles from 'scss/resume.module.scss';
 
 class Resume extends Component {
   state = {
-    resume: null
+    resume: null,
   };
 
   async componentDidMount() {
-    const res = await fetch(resume);
+    const res = await fetch(resumeMD);
     const text = await res.text();
     await this.setState({ resume: text });
   }
@@ -20,7 +20,7 @@ class Resume extends Component {
     const { resume } = this.state;
 
     return (
-      <div className={styles["resume"]}>
+      <div className={styles['resume']}>
         {resume ? <ReactMarkdown source={resume} /> : <Loading />}
       </div>
     );
