e450a060cda7c63558abb22b717cdb4c7b97f8cf
Robert Skwiat <rskwiat@gmail.com>
2019-12-09 23:35:23 -0500

    Updates

---
+++ precache-manifest.24d511c0acf8f611a5f4d60f2739bf98.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9be5e306962a5122d0cca65f0cd4dee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4764184d6804bda5c693",
+    "url": "/static/css/main.44469818.chunk.css"
+  },
+  {
+    "revision": "713bcc13c5352c149554",
+    "url": "/static/js/2.f32369d1.chunk.js"
+  },
+  {
+    "revision": "76d7a8da27b1246d0ec7305dafc313ce",
+    "url": "/static/js/2.f32369d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4764184d6804bda5c693",
+    "url": "/static/js/main.7a03a1d1.chunk.js"
+  },
+  {
+    "revision": "6686637387293b7d4893",
+    "url": "/static/js/runtime-main.936cfb80.js"
+  },
+  {
+    "revision": "6570a1ed7de49467a27ebfb10af0504b",
+    "url": "/static/media/headshot.6570a1ed.png"
+  },
+  {
+    "revision": "11120d056538c23365863a7049aecbaa",
+    "url": "/static/media/icons.11120d05.svg"
+  },
+  {
+    "revision": "f02642561f7e3845d5c63da72ecdadad",
+    "url": "/static/media/resume.f0264256.md"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.188129df043293058e573251708fea8c.js"
+  "/precache-manifest.24d511c0acf8f611a5f4d60f2739bf98.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
